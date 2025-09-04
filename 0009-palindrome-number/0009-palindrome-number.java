a423e6bbb0f08848f54839bc02d3e2c609deae13
Mudar Hussain <mudar.shussain@gmail.com>
2025-09-04 23:39:07 +0530

    Time: 26 ms (5.03%), Space: 44.2 MB (35.49%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int xOriginal = x;
+        int palindrome = 0;
+        while(x>0) {
+            palindrome = palindrome * 10 + x%10;
+            x = x/10;
+        }
+        System.out.print(palindrome);
+        return palindrome == xOriginal;
+    }
+}
