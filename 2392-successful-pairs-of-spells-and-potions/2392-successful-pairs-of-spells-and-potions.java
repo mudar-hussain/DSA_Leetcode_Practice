805bf252094b77ef31152c787e0e7710bc511bc6
Mudar Hussain <mudar.shussain@gmail.com>
2025-10-08 11:50:47 +0530

    Time: 46 ms (69.51%), Space: 69.9 MB (5.27%) - LeetHub

---
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n = spells.length;
+        int m = potions.length;
+        int[] ans = new int[n];
+        Arrays.sort(potions);
+        for (int i = 0; i<n; i++) {
+            int idx = bs(spells[i], potions, success);
+            if (idx!=-1) ans[i] = m-idx;
+        }
+        return ans;
+    }
+    private int bs(int spell, int[] potions, long success) {
+        int low = 0, high = potions.length-1, idx = -1;
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            if ((long) potions[mid] * spell >= success) {
+                idx = mid;
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return idx;
+    }
+}
