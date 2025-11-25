5868342b98079f100fc267a70ae0ffeef701875a
Mudar Hussain <mudar.shussain@gmail.com>
2025-11-25 07:41:13 +0530

    Time: 0 ms (100%), Space: 43.7 MB (19.43%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,39 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(m==0) {
-            while(--n>=0) {
-                nums1[n] = nums2[n];
-            }
-            
-            return;
-        }
-        if(n==0) return;
-        int l = m-1;
-        int r = n-1;
+        int i = m-1, j = n-1;
         int idx = m+n-1;
-        while(idx>=0 && r>=0 && l>=0) {
-            if(nums1[l]>=nums2[r]){
-                nums1[idx] = nums1[l];
-                l--;
+        while(idx>=0 && i>=0 && j>=0) {
+            if (nums2[j] > nums1[i]) {
+                nums1[idx] = nums2[j];
+                j--;
             } else {
-                nums1[idx] = nums2[r];
-                r--;
+                nums1[idx] = nums1[i];
+                i--;
             }
             idx--;
         }
-        if(idx>=0 && l<=0 && r>=0) {
-            while(r>=0) {
-                nums1[idx] = nums2[r];
-                r--;
-                idx--;
-            }
-            
-        } else if (idx>=0 && l>=0 && r<=0) {
-            while(l>=0) {
-                nums1[idx] = nums1[l];
-                l--;
-                idx--;
-            }
+        while(idx>=0 && i>=0) {
+            nums1[idx] = nums1[i];
+            i--;
+            idx--;
+        }
+        while(idx>=0 && j>=0) {
+            nums1[idx] = nums2[j];
+            j--;
+            idx--;
         }
 
     }
