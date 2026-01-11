945c57ce79a86a56e3d0447efe1ae84500228c6e
Mudar Hussain <mudar.shussain@gmail.com>
2026-01-11 14:52:00 +0530

    Time: 24 ms (24.32%), Space: 50.2 MB (13.47%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -3,23 +3,24 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         int[] heights = new int[n];
+        int[] lr = new int[n];
+        int[] rl = new int[n];
+        Stack<Integer> st = new Stack<>();
         int maxArea = 0;
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
                 if(matrix[i][j] == '1') heights[j]+=1;
                 else heights[j] = 0;
             }
-            maxArea = Math.max(maxArea, largestRectangleArea(heights));
+            maxArea = Math.max(maxArea, largestRectangleArea(heights, lr, rl, st));
         }
         return maxArea;
     }
     
-    public int largestRectangleArea(int[] heights) {
+    public int largestRectangleArea(int[] heights, int[] lr, int[] rl, Stack<Integer> st) {
         int n = heights.length;
         int ans = 0;
-        int[] lr = new int[n];
-        int[] rl = new int[n];
-        Stack<Integer> st = new Stack<>();
+        st.clear();
         for(int i = 0; i<n; i++){
             while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
                 st.pop();
