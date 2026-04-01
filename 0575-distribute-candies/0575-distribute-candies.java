ccf93e146aa272af6ed2ec718f872424f3d282fe
Mudar Hussain <mudar.shussain@gmail.com>
2026-04-01 13:56:57 +0530

    Time: 31 ms (55.08%), Space: 48.7 MB (42.4%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i<candyType.length; i++){
+            set.add(candyType[i]);
+        }
+        return Math.min(set.size(), candyType.length/2);
+    }
+}
