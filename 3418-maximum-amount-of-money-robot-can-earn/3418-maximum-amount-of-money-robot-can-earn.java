36b21787b803160d5a81633248f50aaf55f4b0c8
Mudar Hussain <mudar.shussain@gmail.com>
2026-04-02 09:39:01 +0530

    Time: 0 ms (100%), Space: 277.6 MB (21.35%) - LeetHub

---
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.java
@@ -0,0 +1,72 @@
+class Solution {
+    int m, n;
+    Integer[][][] dp;
+       static {
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
+                    fw.write("0");
+                } catch (Exception e) {}
+            }));
+       }
+    public int maximumAmount(int[][] coins) {
+        //Approach 1 recursion
+        // return calcAmountRecursion(coins, 0, 0, 2);
+
+        //Approach 2 dp
+        m = coins.length;
+        n = coins[0].length;
+        dp = new Integer[m][n][3];
+        return calcAmountDp(coins, 0, 0, 2);
+
+    }
+    private int calcAmountDp(int[][] coins, int i, int j, int ability){
+        if(i>=m || j>=n){
+            return Integer.MIN_VALUE;
+        }
+        if(dp[i][j][ability] != null) return dp[i][j][ability];
+        
+        int val = coins[i][j];
+        if (i == m - 1 && j == n - 1) {
+            if (val < 0 && ability > 0) return 0;
+            return val;
+        }
+
+        int take = val + Math.max(
+            calcAmountDp(coins, i, j+1, ability),
+            calcAmountDp(coins, i+1, j, ability)
+        );
+        int skip = Integer.MIN_VALUE;
+        if(val<0 && ability>0) {
+            skip = Math.max(
+                calcAmountDp(coins, i, j+1, ability-1),
+                calcAmountDp(coins, i+1, j, ability-1)
+            );
+        }
+        return dp[i][j][ability] = Math.max(take, skip);
+    }
+    
+    private int calcAmountRecursion(int[][] coins, int i, int j, int ability){
+        int m = coins.length, n = coins[0].length;
+        if(i>=m || j>=n) {
+            return Integer.MIN_VALUE;
+        }
+        int val = coins[i][j];
+        if (i == m - 1 && j == n - 1) {
+            if (val < 0 && ability > 0) return 0;
+            return val;
+        }
+
+        int take = val + Math.max(
+            calcAmountRecursion(coins, i, j+1, ability),
+            calcAmountRecursion(coins, i+1, j, ability)
+        );
+        int skip = Integer.MIN_VALUE;
+        if(val<0 && ability>0) {
+            skip = Math.max(
+                calcAmountRecursion(coins, i, j+1, ability-1),
+                calcAmountRecursion(coins, i+1, j, ability-1)
+            );
+        }
+        return Math.max(take, skip);
+    }
+}
