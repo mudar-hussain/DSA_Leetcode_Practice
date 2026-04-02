d772482282e5178c7c7753bb048af4a92153b52b
Mudar Hussain <mudar.shussain@gmail.com>
2026-04-02 07:41:58 +0530

    Time: 19 ms (98.13%), Space: 49.1 MB (10.92%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -3,6 +3,7 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         for(int i = 0; i<candyType.length; i++){
             set.add(candyType[i]);
+            if(set.size()>candyType.length/2) return candyType.length/2;
         }
         
         return Math.min(set.size(), candyType.length/2);
