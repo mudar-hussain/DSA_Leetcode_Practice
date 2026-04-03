83b95de236d557c765e29c07b126ab1fa7f26471
Mudar Hussain <mudar.shussain@gmail.com>
2026-04-03 10:25:23 +0530

    Time: 137 ms (9.17%), Space: 161.1 MB (68.16%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -1,20 +1,19 @@
 class Solution {
     public int countSubmatrices(int[][] grid, int k) {
         int m = grid.length, n = grid[0].length;
-        int[][] sum = new int[m][n];
         int count = 0;
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                int top = i>0?sum[i-1][j]:0;
-                int left = j>0?sum[i][j-1]:0;
-                int dia = i>0 && j>0? sum[i-1][j-1]:0;
-                sum[i][j] = grid[i][j] + top + left-dia;
-                if(sum[i][j] <= k) count++;
+                int top = i>0?grid[i-1][j]:0;
+                int left = j>0?grid[i][j-1]:0;
+                int dia = i>0 && j>0? grid[i-1][j-1]:0;
+                grid[i][j] = grid[i][j] + top + left-dia;
+                if(grid[i][j] <= k) count++;
             }
         }
         
         for(int i = 0; i<m; i++){
-            System.out.println(Arrays.toString(sum[i]));
+            System.out.println(Arrays.toString(grid[i]));
 
         }
         return count;
