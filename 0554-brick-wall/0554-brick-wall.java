255eb75c106eee1706e4e664a19ae1cbdee60f1b
Mudar Hussain <mudar.shussain@gmail.com>
2026-04-07 08:25:19 +0530

    Time: 13 ms (81.08%), Space: 51 MB (38.72%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.java
+++ 0554-brick-wall/0554-brick-wall.java
@@ -2,12 +2,13 @@ class Solution {
     public int leastBricks(List<List<Integer>> wall) {
         Map<Long, Integer> freq = new HashMap<>();
         long sumLength = 0;
-        int maxIntersection = 0;
+        int maxIntersection = 0, updatedFreq = 0;
+
         for(List<Integer> list: wall){
             sumLength = 0;
-            for(int i= 0; i<list.size()-1; i++){
+            for(int i = 0; i<list.size()-1; i++){
                 sumLength += list.get(i);
-                int updatedFreq = freq.getOrDefault(sumLength,0)+1;
+                updatedFreq = freq.getOrDefault(sumLength,0)+1;
                 freq.put(sumLength, updatedFreq);
                 if(updatedFreq>maxIntersection) 
                     maxIntersection = updatedFreq;
