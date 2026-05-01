3ec11f89b3c9623415331619d6b187d9ef4a4669
Mudar Hussain <mudar.shussain@gmail.com>
2026-05-01 08:19:12 +0530

    Time: 2 ms (100%), Space: 46.4 MB (65.19%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,23 +1,27 @@
 class Solution {
+    private int romanIntLookUp(char ch){
+        switch(ch){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
     public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
 
         int n = s.length();
         char prev = s.charAt(0);
-        int value = map.get(prev);
+        int value = romanIntLookUp(prev);
         for(int i = 1; i<n; i++) {
             char curr = s.charAt(i);
-            if(map.get(prev)<map.get(curr)){
-                value += (map.get(curr)-2*map.get(prev));
+            if(romanIntLookUp(prev)<romanIntLookUp(curr)){
+                value += (romanIntLookUp(curr)-2*romanIntLookUp(prev));
             } else {
-                value += map.get(curr);
+                value += romanIntLookUp(curr);
             }
             prev = curr;
             
