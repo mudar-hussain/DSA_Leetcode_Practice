682bf4537b00b201241f65e7fad5d00930391b9a
Mudar Hussain <mudar.shussain@gmail.com>
2026-05-16 21:07:25 +0530

    Time: 18 ms (16.71%), Space: 47 MB (23.41%) - LeetHub

--- 0321-create-maximum-number/0321-create-maximum-number.java
+++ 0321-create-maximum-number/0321-create-maximum-number.java
@@ -1,48 +1,45 @@
 class Solution {
     private int[] maxSeq(int[] nums, int k) {
+        int[] res = new int[k];
         int drop = nums.length - k;
-        Deque<Integer> st = new ArrayDeque<>();
-
-        for(int num : nums){
-            while(drop > 0 && !st.isEmpty() && st.peekLast() < num){
-                st.pollLast();
+        Deque<Integer> deq = new ArrayDeque<>();
+        for(int num: nums) {
+            while(drop>0 && !deq.isEmpty() && deq.peekLast() < num) {
+                deq.pollLast();
                 drop--;
             }
-            st.addLast(num);
+            deq.addLast(num);
         }
-
-        int[] res = new int[k];
         int i = 0;
-        for(int num : st){
-            if(i == k) break;
+        for(int num: deq) {
+            if(i==k) break;
             res[i++] = num;
         }
-
         return res;
     }
 
-    private boolean greater(int[] a, int i, int[] b, int j) {
-        while(i<a.length && j<b.length && a[i]==b[j]){
+    private boolean greater(int[] nums1, int i, int[] nums2, int j) {
+        while(i<nums1.length && j<nums2.length && nums1[i] == nums2[j]) {
             i++;
             j++;
         }
-        return j==b.length || (i<a.length && a[i]>b[j]);
+        return j==nums2.length || (i<nums1.length && nums1[i]>nums2[j]);
     }
-    private int[] merge(int[] a, int[] b) {
-        int n = a.length+b.length;
-        int[] res = new int[n];
+
+    private int[] merge(int[] nums1, int[] nums2) {
         int i = 0, j = 0, r = 0;
-        while(i<a.length || j<b.length) {
-            res[r++] = greater(a, i, b, j) ? a[i++] : b[j++];
+        int[] res = new int[nums1.length+nums2.length];
+        while(i<nums1.length || j<nums2.length) {
+            res[r++] = greater(nums1, i, nums2, j) ? nums1[i++] : nums2[j++];
         }
         return res;
     }
+
     public int[] maxNumber(int[] nums1, int[] nums2, int k) {
         int[] best = new int[k];
         for(int i = Math.max(0, k-nums2.length); i<=Math.min(k, nums1.length); i++) {
             int[] a = maxSeq(nums1, i);
             int[] b = maxSeq(nums2, k-i);
-
             int[] candidate = merge(a, b);
             best = greater(candidate, 0, best, 0) ? candidate : best;
         }
