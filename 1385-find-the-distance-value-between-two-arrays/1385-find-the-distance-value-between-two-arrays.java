689a151b17f5b142b6f52c3aeb778422105d5676
Mudar Hussain <mudar.shussain@gmail.com>
2026-05-18 13:19:12 +0530

    Time: 6 ms (42.06%), Space: 46.6 MB (17.21%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.java
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.java
@@ -1,14 +1,24 @@
 class Solution {
     private boolean isValid(int[] arr, int k, int d) {
-        int low = 0, high = arr.length-1;
-        while(low<=high) {
-            int mid = low + (high-low)/2;
-            if(Math.abs(arr[low]-k) <= d || Math.abs(arr[mid]-k) <= d || Math.abs(arr[high]-k) <= d) return false;
-            else if (arr[mid]-k > d) high = mid-1;
-            else low = mid+1;
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= k - d && arr[mid] <= k + d) {
+                return false;
+            }
+
+            if (arr[mid] < k - d) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
+
         return true;
     }
+    
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
         Arrays.sort(arr2);
         int res = 0;
