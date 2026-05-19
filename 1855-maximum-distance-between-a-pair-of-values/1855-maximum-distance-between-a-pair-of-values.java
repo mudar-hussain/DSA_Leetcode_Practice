f161b1180cc8fa27bd4b9b8fac49473739cd973b
Mudar Hussain <mudar.shussain@gmail.com>
2026-05-19 13:49:13 +0530

    Time: 3 ms (82.37%), Space: 90 MB (61.66%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.java
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.java
@@ -1,20 +1,33 @@
 class Solution {
-    private int binary(int[] nums, int i, int target) {
-        int low = i, high = nums.length-1;
-        int idx = i-1;
-        while(low<=high) {
-            int mid = low + (high-low)/2;
-            if(nums[mid] >= target) {
-                low = mid+1;
-                idx = mid;
-            } else high = mid-1;
-        }
-        return idx;
-    }
+    // private int binary(int[] nums, int i, int target) {
+    //     int low = i, high = nums.length-1;
+    //     int idx = i-1;
+    //     while(low<=high) {
+    //         int mid = low + (high-low)/2;
+    //         if(nums[mid] >= target) {
+    //             low = mid+1;
+    //             idx = mid;
+    //         } else high = mid-1;
+    //     }
+    //     return idx;
+    // }
+    // public int maxDistance(int[] nums1, int[] nums2) {
+    //     int maxDist = 0;
+    //     for(int i = 0; i<nums1.length; i++){
+    //         maxDist = Math.max(maxDist, binary(nums2, i, nums1[i])-i);
+    //     }
+    //     return maxDist;
+
+    // }
     public int maxDistance(int[] nums1, int[] nums2) {
         int maxDist = 0;
-        for(int i = 0; i<nums1.length; i++){
-            maxDist = Math.max(maxDist, binary(nums2, i, nums1[i])-i);
+        int i = 0, j = 0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]<=nums2[j]) {
+                maxDist = Math.max(maxDist, j-i);
+                j++;
+            } else i++;
+
         }
         return maxDist;
 
