45cef621bed5d35e73f406f9587e27f932c17819
Mudar Hussain <mudar.shussain@gmail.com>
2026-05-21 10:46:14 +0530

    Time: 1 ms (100%), Space: 48.9 MB (31.57%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,70 +1,31 @@
 class Solution {
-
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1.length>nums2.length) return findMedianSortedArrays(nums2, nums1);
+        int n = nums1.length, m = nums2.length;
+        int low = 0, high = n;
+        while(low<=high) {
+            int partNums1 = low + (high-low)/2;
+            int partNums2 = (n+m+1)/2 - partNums1;
 
-        // Always binary search smaller array
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
+            int x = partNums1 == 0 ? Integer.MIN_VALUE : nums1[partNums1-1];
+            int y = partNums1 == n ? Integer.MAX_VALUE : nums1[partNums1];
 
-        int x = nums1.length;
-        int y = nums2.length;
+            int p = partNums2 == 0 ? Integer.MIN_VALUE : nums2[partNums2-1];
+            int q = partNums2 == m ? Integer.MAX_VALUE : nums2[partNums2];
 
-        int low = 0;
-        int high = x;
-
-        while (low <= high) {
-
-            int partitionX = low + (high - low) / 2;
-
-            int partitionY = (x + y + 1) / 2 - partitionX;
-
-            int maxLeftX =
-                (partitionX == 0)
-                ? Integer.MIN_VALUE
-                : nums1[partitionX - 1];
-
-            int minRightX =
-                (partitionX == x)
-                ? Integer.MAX_VALUE
-                : nums1[partitionX];
-
-            int maxLeftY =
-                (partitionY == 0)
-                ? Integer.MIN_VALUE
-                : nums2[partitionY - 1];
-
-            int minRightY =
-                (partitionY == y)
-                ? Integer.MAX_VALUE
-                : nums2[partitionY];
-
-            if (maxLeftX <= minRightY &&
-                maxLeftY <= minRightX) {
-
-                // Even total length
-                if ((x + y) % 2 == 0) {
-
-                    return (
-                        Math.max(maxLeftX, maxLeftY)
-                        +
-                        Math.min(minRightX, minRightY)
-                    ) / 2.0;
+            if(x<=q && p<=y) {
+                if((m+n)%2 == 0) {
+                    return ((double)Math.max(x, p) + (double)Math.min(y, q))/2.0;
+                } else {
+                    return (double)Math.max(x, p);
                 }
-
-                // Odd total length
-                return Math.max(maxLeftX, maxLeftY);
-            }
-
-            else if (maxLeftX > minRightY) {
-                high = partitionX - 1;
-            }
-
-            else {
-                low = partitionX + 1;
+            } else if(x > q) {
+                high = partNums1 - 1;
+            } else {
+                low = partNums1 + 1;
             }
         }
-
         return 0.0;
+        
     }
 }
