ad221b428b96d94452f8d8422ae6744a71e0121d
Mudar Hussain <mudar.shussain@gmail.com>
2026-06-02 09:20:17 +0530

    Time: 29 ms (50.35%), Space: 70 MB (39.3%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.java
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.java
@@ -3,22 +3,20 @@ class Solution {
         int count = 0;
         for(int n: nums) {
             count += (n-1)/penalty;
-            if(count>maxOps) break;
+            if(count>maxOps) return false;
         }
-        return count<=maxOps;
+        return true;
     }
     public int minimumSize(int[] nums, int maxOperations) {
         int low = 1, high = Arrays.stream(nums).max().getAsInt();
-        int ans = high;
-        while(low<=high) {
+        while(low<high) {
             int mid = low + (high-low)/2;
             if(isValidOperations(nums, mid, maxOperations)) {
-                ans = mid;
-                high = mid - 1;
+                high = mid;
             } else {
                 low = mid + 1;
             }
         }
-        return ans;
+        return high;
     }
 }
