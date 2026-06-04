7ec93e13d459c8d99736ee2413c6827fa41923ef
Mudar Hussain <mudar.shussain@gmail.com>
2026-06-04 08:32:02 +0530

    Time: 1 ms (94.24%), Space: 43.5 MB (56.24%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,14 +1,9 @@
 class Solution {
-    public String gcdOfStrings(String str1, String str2) {
-        // Check if concatenated strings are equal or not, if not return ""
-        if (!(str1 + str2).equals(str2 + str1))
-            return "";
-        // If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
-        int gcd = gcd(str1.length(), str2.length());
-        return str1.substring(0, gcd);
-    }
-
     private int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
+        return b==0? a : gcd(b, a%b);
+    }
+    public String gcdOfStrings(String str1, String str2) {
+        if(!((str1+str2).equals(str2+str1))) return "";
+        return str1.substring(0, gcd(str1.length(), str2.length()));
     }
 }
