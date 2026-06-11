c4e1bee4438a75e287ed315136c6484c10dfeef0
Mudar Hussain <mudar.shussain@gmail.com>
2026-06-11 15:19:00 +0530

    Time: 36 ms (19.69%), Space: 91.8 MB (82.29%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,22 @@
+class Solution {
+    private boolean isPossible(int[] candies, long k, int divisor) {
+        long childrens = 0;
+        for(int c: candies) {
+            childrens += (long)c/divisor;
+            if(childrens>=k) return true;
+        }
+        return false;
+    }
+    public int maximumCandies(int[] candies, long k) {
+        int low = 1, high = Arrays.stream(candies).max().getAsInt();
+        int ans = 0;
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+            if(isPossible(candies, k, mid)) {
+                ans = mid;
+                low = mid+1;
+            } else high = mid-1;
+        }
+        return ans;
+    }
+}
