76a60bc6b816e058e24b8b175ee932b6ec9185b1
Mudar Hussain <mudar.shussain@gmail.com>
2026-07-21 09:55:06 +0530

    Time: 39 ms (80.37%), Space: 46.9 MB (52.34%) - LeetHub

--- 2059-minimum-operations-to-convert-number/2059-minimum-operations-to-convert-number.java
+++ 2059-minimum-operations-to-convert-number/2059-minimum-operations-to-convert-number.java
@@ -1,24 +1,28 @@
 class Solution {
     public int minimumOperations(int[] nums, int start, int goal) {
-        int res = 0;
         Queue<Integer> q = new LinkedList<>();
-        Set<Integer> set = new HashSet<>();
         q.offer(start);
+        boolean[] visited = new boolean[1001];
+        if(start>=0 && start<=1000) visited[start] = true;
+        int steps = 0;
         while(!q.isEmpty()) {
             int size = q.size();
             while(size-->0) {
                 int x = q.poll();
-                if(x==goal) return res;
-                if((x<0 || x>1000) || set.contains(x)) continue;
-                if(!set.contains(x)) set.add(x);
-                for(int num: nums) {
-                    q.offer(x+num);
-                    q.offer(x-num);
-                    q.offer(x^num);
+                for(int n: nums) {
+                    int[] next = {x+n, x-n, x^n};
+                    for(int y: next) {
+                        if(y==goal) return steps+1;
+                        if(y>=0 && y<=1000 && !visited[y]) {
+                            q.offer(y);
+                            visited[y] = true;
+                        }
+                    }
                 }
             }
-            res++;
+            steps++;
         }
         return -1;
+        
     }
 }
