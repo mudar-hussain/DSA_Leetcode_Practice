3fdfa0100236d3cda2d22de1405cc9582be33c71
Mudar Hussain <mudar.shussain@gmail.com>
2026-08-09 19:33:52 +0530

    Time: 0 ms (100%), Space: 44 MB (89.03%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -12,30 +12,38 @@ class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1 == null) return list2;
         else if(list2 == null) return list1;
-        ListNode head;
         if(list1.val<=list2.val) {
-            head = list1;
-            list1 = list1.next;
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         } else {
-            head = list2;
-            list2 = list2.next;
-        }
-        ListNode pt = head;
-        while(list1!=null && list2!=null) {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
 
-            if(list1.val<=list2.val) {
-                pt.next = list1;
-                pt = pt.next;
-                list1 = list1.next;
-            } else {
-                pt.next = list2;
-                pt = pt.next;
-                list2 = list2.next;
-            }
         }
-        if(list1!=null) pt.next = list1;
-        else if(list2!=null) pt.next = list2;
-        return head;
+        // ListNode head;
+        // if(list1.val<=list2.val) {
+        //     head = list1;
+        //     list1 = list1.next;
+        // } else {
+        //     head = list2;
+        //     list2 = list2.next;
+        // }
+        // ListNode pt = head;
+        // while(list1!=null && list2!=null) {
+
+        //     if(list1.val<=list2.val) {
+        //         pt.next = list1;
+        //         pt = pt.next;
+        //         list1 = list1.next;
+        //     } else {
+        //         pt.next = list2;
+        //         pt = pt.next;
+        //         list2 = list2.next;
+        //     }
+        // }
+        // if(list1!=null) pt.next = list1;
+        // else if(list2!=null) pt.next = list2;
+        // return head;
         
     }
 }
