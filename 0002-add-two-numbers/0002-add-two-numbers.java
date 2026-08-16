cc6a26f866312f49798983d5f6f2f9c9229060ef
Mudar Hussain <mudar.shussain@gmail.com>
2026-08-16 23:43:19 +0530

    Time: 1 ms (100%), Space: 46.6 MB (38.32%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -9,24 +9,44 @@
  * }
  */
 class Solution {
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null, curr = head, next = head.next;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode ans = new ListNode();
-        int carryForward = 0;
-        ListNode prev = ans;
-        while(l1 != null || l2 != null){
-            int sum = (l1!=null?l1.val:0) + (l2!=null?l2.val:0) + carryForward;
-            carryForward = sum/10;
-            sum = sum%10;
-            ListNode node = new ListNode(sum);
-            prev.next = node;
-            prev = node;
-            l1 = l1!=null?l1.next:l1;
-            l2 = l2!=null?l2.next:l2;
+        if(l1.next==null && l1.val == 0) return l2;
+        if(l2.next==null && l2.val == 0) return l1;
+        // l1 = reverse(l1);
+        // l2 = reverse(l2);
+        int carryOver = 0;
+        ListNode res = new ListNode(0), curr = res;
+
+        while(l1!=null || l2!=null) {
+            int sum = 0;
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            };
+            sum += carryOver;
+            curr.next = new ListNode(sum%10);
+            carryOver = sum/10;
+            curr = curr.next;
         }
-        if (carryForward > 0) {
-            ListNode node = new ListNode(carryForward);
-            prev.next = node;
+        while(carryOver > 0) {
+            curr.next = new ListNode(carryOver%10);
+            carryOver /= 10;
         }
-        return ans.next;
+        return res.next;
     }
 }
