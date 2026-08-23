06a045fa3b9ebb706428e03a139b33035cc1aad7
Mudar Hussain <mudar.shussain@gmail.com>
2026-08-23 16:42:56 +0530

    Time: 32 ms (30.07%), Space: 47.4 MB (95.08%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,16 +1,26 @@
 class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int start = 0;
-        int end = arr.length-1;
-        while(end-start>=k){
-            if(Math.abs(arr[start]-x)>Math.abs(arr[end]-x)){
-                start++;
-            } else end--;
-        }    
-        List<Integer> ans= new ArrayList<>();
-        while(start<=end) {
-            ans.add(arr[start++]);
+    class Pair {
+        int val;
+        int dist;
+        public Pair (int val, int dist) {
+            this.val = val;
+            this.dist = dist;
         }
-        return ans;
+    }
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        Queue<Pair> pq = new PriorityQueue<>((a,b) -> {
+            if(a.dist == b.dist) return a.val-b.val;
+            else return a.dist - b.dist;
+        });
+        for(int i: arr) {
+            pq.offer(new Pair(i, Math.abs(x-i)));
+        }
+        Integer[] res = new Integer[k];
+        for(int i = 0; i<k; i++){
+            res[i] = pq.poll().val;
+        }
+        Arrays.sort(res);
+        return new ArrayList<>(Arrays.asList(res));
+        
     }
 }
