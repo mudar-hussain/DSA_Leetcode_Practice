c4029b0bda4701bfca0d9286f5848221d038a1d8
Mudar Hussain <mudar.shussain@gmail.com>
2026-09-04 07:24:52 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.02%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -7,6 +7,6 @@ class Solution {
     private int solve(int n) {
         if(n==0 || n==1) return n;
         if(dp[n]!=-1) return dp[n];
-        return dp[n] = fib(n-1)+fib(n-2);
+        return dp[n] = solve(n-1)+solve(n-2);
     }
 }
