894513ffe51f29641ffa9048c792c79ad8c674a1
Mudar Hussain <mudar.shussain@gmail.com>
2026-09-04 14:38:42 +0530

    Time: 234 ms (5.16%), Space: 123.6 MB (5.24%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -6,10 +6,10 @@ class Solution {
         Queue<int[]> minpq = new PriorityQueue<>((a,b) -> a[0]!=b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
         Queue<int[]> maxpq = new PriorityQueue<>((a,b) -> a[0]!=b[0] ? Integer.compare(b[0], a[0]) : Integer.compare(a[1], b[1]));
         while(right<n) {
-            minpq.offer(new int[]{nums[right], right});
-            maxpq.offer(new int[]{nums[right], right});
             while(!minpq.isEmpty() && minpq.peek()[1]<left) minpq.poll();
             while(!maxpq.isEmpty() && maxpq.peek()[1]<left) maxpq.poll();
+            minpq.offer(new int[]{nums[right], right});
+            maxpq.offer(new int[]{nums[right], right});
             if(Math.abs(minpq.peek()[0]-maxpq.peek()[0]) > limit) {
                 left++;
             } else {
