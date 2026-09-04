ff6bae00e26ba9022aee84474be9f440b9c0b996
Mudar Hussain <mudar.shussain@gmail.com>
2026-09-04 14:41:27 +0530

    Time: 154 ms (5.16%), Space: 102.2 MB (7.93%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -6,16 +6,19 @@ class Solution {
         Queue<int[]> minpq = new PriorityQueue<>((a,b) -> a[0]!=b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
         Queue<int[]> maxpq = new PriorityQueue<>((a,b) -> a[0]!=b[0] ? Integer.compare(b[0], a[0]) : Integer.compare(a[1], b[1]));
         while(right<n) {
-            while(!minpq.isEmpty() && minpq.peek()[1]<left) minpq.poll();
-            while(!maxpq.isEmpty() && maxpq.peek()[1]<left) maxpq.poll();
             minpq.offer(new int[]{nums[right], right});
             maxpq.offer(new int[]{nums[right], right});
-            if(Math.abs(minpq.peek()[0]-maxpq.peek()[0]) > limit) {
-                left++;
-            } else {
-                res = Math.max(res, right-left+1);
-                right++;
+            while(true) {
+                while(!minpq.isEmpty() && minpq.peek()[1]<left) minpq.poll();
+                while(!maxpq.isEmpty() && maxpq.peek()[1]<left) maxpq.poll();
+                if(Math.abs(minpq.peek()[0]-maxpq.peek()[0]) > limit) {
+                    left++;
+                } else {
+                    break;
+                }
             }
+            res = Math.max(res, right-left+1);
+            right++;
         }
         return res;
 
