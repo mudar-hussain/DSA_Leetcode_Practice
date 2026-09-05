fc43e2d5d0a40b69b9ddcec9d61b7f894d85b7a4
Mudar Hussain <mudar.shussain@gmail.com>
2026-09-05 11:03:15 +0530

    Time: 8 ms (94.93%), Space: 61.8 MB (99.52%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,16 +1,16 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] freq = new int[fruits.length];
         int left = 0, distinct = 0;
         int maxFruits = 0;
         for(int right = 0; right<fruits.length; right++) {
-            map.put(fruits[right], map.getOrDefault(fruits[right],0)+1);
-            if(map.get(fruits[right]) == 1) {
+            freq[fruits[right]]++;
+            if(freq[fruits[right]] == 1) {
                 distinct++;
             }
             while(left<right && distinct > 2) {
-                map.put(fruits[left], map.get(fruits[left])-1);
-                if(map.get(fruits[left++]) == 0) {
+                freq[fruits[left]]--;
+                if(freq[fruits[left++]] == 0) {
                     distinct--;
                 }
             }
